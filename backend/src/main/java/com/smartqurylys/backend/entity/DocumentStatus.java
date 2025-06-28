e4c05d5f8d1b4c4d4719ab2e4f3bd8d951e1dc8a
chesnoqq <166210810+chesnoqq@users.noreply.github.com>
2025-06-28 15:38:38 +0500

    Update and rename Document.Status to DocumentStatus.java

---
+++ backend/src/main/java/com/smartqurylys/backend/entity/DocumentStatus.java
@@ -0,0 +1,12 @@
+package com.smartqurylys.entity;
+
+public enum DocumentStatus {
+    WAITING,
+    CREATION,
+    APPROVAL,
+    SIGNATURE,
+    REGISTRATION,
+    IN_PROGRESS,
+    COMPLETED
+}
+
