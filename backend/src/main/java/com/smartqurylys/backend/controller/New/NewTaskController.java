492f0aa02bb757aab0aad4013079b492f4ac2086
Lagman54 <aibar.kydiarxan@gmail.com>
2026-03-13 19:26:38 +0500

    Add new task controller

---
+++ backend/src/main/java/com/smartqurylys/backend/controller/New/NewTaskController.java
@@ -0,0 +1,52 @@
+package com.smartqurylys.backend.controller.New;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.smartqurylys.backend.dto.project.task.CreateTaskRequest;
+import com.smartqurylys.backend.dto.project.task.TaskResponse;
+import com.smartqurylys.backend.service.TaskService;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class NewTaskController {
+
+    private final TaskService taskService;
+
+    // Получение задачи по ID.
+    @GetMapping("/tasks/{taskId}")
+    public ResponseEntity<TaskResponse> getTask(
+            @PathVariable Long taskId) {
+        TaskResponse response = taskService.getTaskById(taskId);
+        return ResponseEntity.ok(response);
+    }
+
+    // Получение ВСЕХ задач ПРОЕКТА
+    @GetMapping("projects/{projectId}/tasks")
+    public ResponseEntity<List<TaskResponse>> getAllTasks(@PathVariable Long projectId) {
+        List<TaskResponse> tasks = taskService.getTasksByProject(projectId);
+        return ResponseEntity.ok(tasks);
+    }
+
+    // Получить число задач для данного проекта
+    @GetMapping("projects/{projectId}/tasks/count")
+    public ResponseEntity<Integer> getTasksCount(@PathVariable Long projectId) {
+        int count = taskService.getTasksCount(projectId);
+        return ResponseEntity.ok(count);
+    }
+}
