9e499cf0d714518b02ee3ddd80eb5fcadf103d55
Lagman54 <aibar.kydiarxan@gmail.com>
2026-04-10 14:04:18 +0500

    add home page for unauth users; update register page

--- frontend/smartqurylys/src/app/home/home.component.ts
+++ frontend/smartqurylys/src/app/home/home.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router'; // Добавляем RouterModule
-import { AuthService } from '../auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Важно: импортируем RouterModule для routerLink
-  templateUrl: './home.component.html', // Указываем HTML-файл
-  styleUrls: ['./home.component.css'] // Указываем CSS-файл
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
-    // Если пользователь аутентифицирован, перенаправляем на дашборд
     if (this.authService.isAuthenticated()) {
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(['/projects']);
     }
-    // Если не аутентифицирован, компонент просто отобразит свой HTML (лендинговую страницу)
   }
 }
