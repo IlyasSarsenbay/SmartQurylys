04d31ef62d35f9305810ab9aa9ed7faf469d3a97
Lagman54 <aibar.kydiarxan@gmail.com>
2026-04-14 10:38:54 +0500

    Clean up sql files

--- backend/check_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Проверка текущего ограничения на поле type в таблице notifications
-SELECT conname, pg_get_constraintdef(oid) 
-FROM pg_constraint 
-WHERE conrelid = 'notifications'::regclass 
-AND contype = 'c';
--- backend/update_notification_type_constraint.sql
+++
@@ -1,6 +0,0 @@
--- Удаляем старый constraint
-ALTER TABLE notifications DROP CONSTRAINT IF EXISTS notifications_type_check;
-
--- Добавляем новый constraint с типом STAGE_REACTIVATION
-ALTER TABLE notifications ADD CONSTRAINT notifications_type_check 
-CHECK (type IN ('INVITATION', 'PROJECT_UPDATE', 'FILE_UPLOAD', 'TASK_ASSIGNMENT', 'MENTION', 'STAGE_REACTIVATION'));
